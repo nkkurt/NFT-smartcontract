2beebc010bf7f25633d17aead3c48366f177a6c0
nkkurt <nazan@pureple.com>
2021-10-01 17:46:56 -0700

    Initial smart contract to min on chain NFTs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require('dotenv').config({ path: __dirname+'/process.env'});
 
-// CAUTION !!! Don't push this file.
-
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -10,13 +10,7 @@ const main = async () => {
     let txn = await nftContract.makeAnEpicNFT()
     //Wait for it to be mined
     await txn.wait()
-    console.log("Minted NFT #1")
-
-    // Mint another NFT for fun
-    // txn = await nftContract.makeAnEpicNFT()
-    //Wait for it to be mined
-    //await txn.wait()
-    //console.log("Minted NFT #2");
+    console.log("Minted an NFT")
     
 };
 
